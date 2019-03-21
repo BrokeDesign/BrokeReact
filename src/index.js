03fd91db77912073210c74305d7c93627b6ab884
Christian Gorke <cgorke@aplura.com>
2019-03-21 17:16:56 -0600

    work on exports for lib

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-export { Card, CardContent, CardFoot, CardHead } from './lib/card';
-export { Col, Grid, Row }from './lib/grid';
-export { Footer, Header } from './lib/layout';
+export * from './lib/card';
+export * from './lib/grid';
+export * from './lib/layout';
