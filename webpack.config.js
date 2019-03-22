c2b191f0b319976166a893cf31248738edd07fa3
Christian Gorke <cgorke@aplura.com>
2019-03-22 02:19:18 -0600

    changed library value

--- webpack.config.js
+++ webpack.config.js
@@ -4,78 +4,102 @@ const InterpolateHtmlPlugin = require('interpolate-html-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const nodeExternals = require('webpack-node-externals');
 
-const devMode = process.env.NODE_ENV !== 'production'
+const devMode = process.env.NODE_ENV !== 'production';
 const path = require('path');
 
 module.exports = {
-    entry: ['./src/index.js', './src/styles/scss/styles.scss'],
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'brokeReact.js',
-        publicPath: '/',
-        libraryTarget: 'commonjs',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: [{ loader: 'babel-loader' }],
-            },
-            {
-                test: /\.html$/,
-                use: [{ loader: 'html-loader', options: { minimize: true } }],
-            },
-            {
-                test: /\.s?(a|c)ss$/,
-                exclude: /node_modules/,
-                use: [
-                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.(png|jpe?g)/i,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            name: './src/assets/images/[name].[ext]',
-                            limit: 10000,
-                        },
-                    },
-                    {
-                        loader: 'img-loader',
-                    },
-                ],
-            },
-        ],
-    },
-    devServer: {
-        contentBase: ['./public', './src'],
-        watchContentBase: true,
-        inline: true,
-        port: 3000,
-        historyApiFallback: true,
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            favicon: './public/favicon.svg',
-        }),
-        // Makes some environment variables available in index.html.
-        // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
-        // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
-        // In development, this will be an empty string.
-        devMode ? new InterpolateHtmlPlugin({
-            PUBLIC_URL: '',
-        }) : console.log('Success: Skipping InterpolateHtmlPlugin for production'),
-        new MiniCssExtractPlugin({
-            filename: devMode ? '[name].css' : '[name].[hash].css',
-            chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
-        }),
-    ],
-    externals: [nodeExternals()]
+	entry: ['./src/index.js', './src/styles/scss/styles.scss'],
+	output: {
+		path: path.join(__dirname, '/dist'),
+		filename: 'brokeReact.js',
+		library: 'brokeReact',
+		libraryTarget: 'umd',
+		publicPath: '/dist/',
+		umdNamedDefine: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: [{ loader: 'babel-loader' }],
+			},
+			{
+				test: /\.html$/,
+				use: [{ loader: 'html-loader', options: { minimize: true } }],
+			},
+			{
+				test: /\.s?(a|c)ss$/,
+				exclude: /node_modules/,
+				use: [
+					devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+					'css-loader',
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.(png|jpe?g)/i,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							name: './src/assets/images/[name].[ext]',
+							limit: 10000,
+						},
+					},
+					{
+						loader: 'img-loader',
+					},
+				],
+			},
+		],
+	},
+	devServer: {
+		contentBase: ['./public', './src'],
+		watchContentBase: true,
+		inline: true,
+		port: 3000,
+		historyApiFallback: true,
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+			favicon: './public/favicon.svg',
+		}),
+		// Makes some environment variables available in index.html.
+		// The public URL is available as %PUBLIC_URL% in index.html, e.g.:
+		// <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
+		// In development, this will be an empty string.
+		devMode
+			? new InterpolateHtmlPlugin({
+					PUBLIC_URL: '',
+			  })
+			: console.log('Success: Skipping InterpolateHtmlPlugin for production'),
+		new MiniCssExtractPlugin({
+			filename: devMode ? '[name].css' : '[name].[hash].css',
+			chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+		}),
+	],
+	resolve: {
+		alias: {
+			react: path.resolve(__dirname, './node_modules/react'),
+			'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+		},
+	},
+	externals: {
+		// Don't bundle react or react-dom
+		react: {
+			commonjs: 'react',
+			commonjs2: 'react',
+			amd: 'React',
+			root: 'React',
+		},
+		'react-dom': {
+			commonjs: 'react-dom',
+			commonjs2: 'react-dom',
+			amd: 'ReactDOM',
+			root: 'ReactDOM',
+		},
+	},
 };
