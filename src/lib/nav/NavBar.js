ed19faf1d083f65952ab50b18e3f404b92714def
Christian Gorke <dev.cmgorke@gmail.com>
2019-04-04 12:16:44 -0600

    work on conditional rendering of rows for navbar creating new branch after this commit.

--- src/lib/nav/NavBar.js
+++ src/lib/nav/NavBar.js
@@ -1,40 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Grid, Row } from '../../';
+import { Grid, Row, Nav } from '../../';
 
 
-const NavBar = props => {
-    // TODO: create sass map for nav with classes: navbar, nav, navfoot, nav-position-(top|side|side-right)
-    // TODO: create scss mixin for nav map
-    return (
-    <div className={`navbar nav nav-position-${props.orient}`}>
-		<Grid>
-			{props.children}
-		</Grid>
-    </div>
-    );
+
+const NavBar = (props) => {
+    isNavItem = props.isNavItem;
+
+	return (
+		// TODO: create sass map for nav with classes: navbar, nav, navfoot, nav-position-(top|side|side-right)
+		// TODO: create scss mixin for nav map
+        <div 
+            className={`navbar ${props.className}`} 
+            sidebar={sidebar} footer={footer} color={color}>
+			<Grid>
+				{isNavItem && (
+                    <Nav>
+                        {props.children}
+                    </Nav>
+                )}
+			</Grid>
+		</div>
+	);
 };
 
 NavBar.propTypes = {
-    /**
-     * orientation of the navbar
-     * side defaults to left - what I imagine is convention
-     * side right goes right and top is the default, 
-     * so no need to use prop if you intend to have a top navbar
-     */
-    orient: PropTypes.oneOf(['side', 'side-right', 'top']),
-    /**
-     * Just the children of the component, recommend using NavItem,
-     * since everything is in Grid (which is just a container), you should either use
-     * row and columns with what you want or you can simply use the Nav components.
-     * TODO: Create remaining Nav Components
-     */
-    children: PropTypes.any, 
+	/**
+	 * orientation of the navbar
+	 * side defaults to left - what I imagine is convention
+	 * side right goes right and top is the default,
+	 * so no need to use prop if you intend to have a top navbar
+	 */
+	orient: PropTypes.oneOf(['side', 'side-right', 'top']),
+	/**
+	 * Just the children of the component, recommend using NavItem,
+	 * since everything is in Grid (which is just a container), you should either use
+	 * row and columns with what you want or you can simply use the Nav components.
+	 * TODO: Create remaining Nav Components
+	 */
+	children: PropTypes.any,
 };
 
 NavBar.defaultProps = {
-    orient: 'top',
+	orient: 'top',
 	children: '',
 };
 
